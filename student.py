3419f656280cc009dd7829dad0cfe10fe69880e4
Zihao Guo <91563230+JackGuo1110@users.noreply.github.com>
2021-12-14 20:11:09 +0000

    a

--- student.py
+++ student.py
@@ -41,7 +41,8 @@ class Piggy(PiggyParent):
                 "f": ("Follow", self.follow),
                 "c": ("Calibrate", self.calibrate),
                 "q": ("Quit", self.quit),
-                "j": ("Jack Test", self.jack)
+                "j": ("Jack Test", self.jack),
+                "a": ("scan", self.scan)
                 }
         # loop and print the menu...
         for key in sorted(menu.keys()):
